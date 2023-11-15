924d004b14795892288d54fa7fa2b921e09fefe0
alinatelychko <adelin9999@gmail.com>
2023-11-15 19:39:59 +0100

    2.4 2nd try

--- index.js
+++ index.js
@@ -4,15 +4,47 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-// Morgan
-app.use(morgan('common'));
+
+
+let topMovies = [
+  {
+    title: 'Inception',
+    director: 'J.K. Rowling'
+  },
+  {
+    title: 'Under The Shadow ',
+    director: 'Babak Anvari'
+  },
+  {
+    title: 'Twilight',
+    director: 'Stephanie Meyer'
+  },
+  {
+    title: 'The Ritual',
+    director: 'David Brucker'
+  },
+  {
+    title: 'The Platform',
+    director: 'Babak Anvari'
+  },
+  {
+    title: 'Oat Studios ',
+    director: 'Neil Bomkamp'
+  }
+];
+
+
 
 // Middleware for parsing requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-// GET requests
+
+app.use(express.static('public'));
+
+app.use(morgan('common'));
+
 app.get('/', (req, res) => {
   res.send('Welcome to my best 10 movies list!');
 });
@@ -21,11 +53,13 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
 app.get('/secreturl', (req, res) => {
   res.send('This is a secret URL with super top-secret content.');
 });
-
-app.use('/documentation', express.static('public'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
