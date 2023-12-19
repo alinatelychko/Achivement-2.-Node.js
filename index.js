d7a22ced6ff2c544231e0d349a7bae3ca5746133
alinatelychko <adelin9999@gmail.com>
2023-12-19 15:07:18 +0100

    movies authentication Re-enabled

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.get("/users/:Username", passport.authenticate('jwt', { session: false }), as
     });
 });
 
-app.get("/movies", async (req, res) => {
+app.get("/movies", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
